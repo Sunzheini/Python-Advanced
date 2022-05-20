ca2a8c4a7b299c10b3495f58647b9c9de0d95d3b
Sunzheini <daniel_zorov@abv.bg>
2022-05-20 17:22:57 +0300

    second commit

---
+++ Lab/L16052022.py
@@ -0,0 +1,215 @@
+
+#tova sa stukturi ot danni
+#stack: lifo
+#queue: filo
+#priority queue -> most optimal element out
+
+#data structure types:
+#linear data structures: list, queue, stack (linear are on 1 line)
+#hash sets: dictionaries, sets
+#trees: binary, RBT, A* trees
+#graphs
+
+#stack - on top of each other, and only can access the last one / top one
+
+# push --> add element to top of stack
+# pop --> removes top elements
+# peek --> vryshta no ne iztriva top element
+# len --> count of elements in stack
+
+# in pyhton we use lists as stacks - mylist = []
+# push - append()
+# pop - pop()
+# peek - print(mylist[-1])
+# len - len(mylist)
+
+# primer - stack implemented as list, resizing array
+# s = []
+# s.append(1)
+# s.append(2)
+# s.append(3)
+# s.append(4)
+# print(s[-1])
+# s.pop()
+# print(s[-1])
+# s.append(9)
+# print(s[-1])
+
+#other variant - stack implemented as linked list
+
+# s = [1, 2, 3, 4, 5]
+# s.insert(2, 11) # not a stack operation
+# also the operations reverse, count, sort, pop(0), s[0]
+
+#----------------------------------------------------------------#
+
+# class Stack:
+#     def __init__(self):
+#         self.values = []
+#
+#     def push(self, value):
+#         self.values.append(value)
+#
+#     def pop(self):
+#         return self.values.pop()
+#
+#     def peek(self):
+#         return self.values[-1]
+#
+#     def count(self):
+#         return len(self.values)
+#
+# s = Stack()
+#
+# for i in range(5, 10):
+#     s.push(i)
+#
+# print(s.pop())
+# print(s.peek())
+# print(s.count())
+
+#----------------------------------------------------------------#
+
+# 1
+# mystring = input()
+#
+# s = []
+# for i in mystring:
+#     s.append(i)
+#
+# reversed_string = ''
+#
+# while s:
+#     reversed_string += s.pop()
+#
+# print(reversed_string)
+
+# 2
+# '1 + (2 - (2 + 3) * 4 / (3 + 1)) * 5'
+# expression = input()
+# s = []
+#
+# for i in range(len(expression)):
+#     if expression[i] == '(':
+#         s.append(i)
+#     elif expression[i] == ')':
+#         start_index = s.pop()
+#         end_index = i + 1
+#         print(expression[start_index:end_index])
+
+# raznovidnosti na 2:
+# is expression valid - stack should be empty at the end
+# when '(' + 1, when ')' -1 and if t the end is 0 then exp is valid
+
+#----------------------------------------------------------------#
+# import time
+#
+# start = time.time()
+
+#QUEUES - FIFO
+#----------------------------------------------------------------#
+
+# from collections import deque
+# #for queues we have deque
+# q = deque()
+#
+# # enqueue
+# q.append(2) #append right
+# # ili q.appendleft(3)
+#
+# # dequeue
+# q.popleft()
+# # ili q.pop() #pop right
+#
+# # peek
+# print(q[0])
+#
+# # count
+
+# 3
+# from collections import deque
+#
+# q = deque()
+#
+# while 1:
+#     command = input()
+#     if command == 'End':
+#         print(f'{len(q)} people remaining.')
+#         break
+#     elif command == 'Paid':
+#         while q:
+#             print(q.popleft())
+#     else:
+#         q.append(command)
+
+# 4
+# from collections import deque
+#
+# people = deque()
+# water_quantity = int(input())
+#
+# while 1:
+#     command = input()
+#     if command == 'Start':
+#         break
+#     else:
+#         people.append(command)
+#
+# while 1:
+#     command = input()
+#     if command == 'End':
+#         break
+#
+#     elif 'refill' in command:
+#         explode = command.split(' ')
+#         water_quantity += int(explode[1])
+#
+#     else:
+#         needed_liters = int(command)
+#         if needed_liters <= water_quantity:
+#             print(f'{people.popleft()} got water')
+#             water_quantity -= needed_liters
+#         else:
+#             print(f'{people.popleft()} must wait')
+#
+# print(f'{water_quantity} liters left')
+
+#----------------------------------------------------------------#
+
+#command.startswith('fff') # proverqva dali zapochva s tova
+#command.endswith
+
+#----------------------------------------------------------------#
+
+# 5
+# import collections
+#
+# kids = collections.deque()
+#
+# list_of_people = input().split(' ')
+# tosses_count = int(input())
+#
+# for i in list_of_people:
+#     kids.append(i)
+#
+# current_count = 0
+# while len(kids) > 1:
+#     current_count += 1
+#     kid = kids.popleft()
+#     if current_count < tosses_count:
+#         kids.append(kid)
+#     else:
+#         print(f'Removed {kid}')
+#         current_count = 0
+#
+# print(f"Last is {kids[0]}")
+
+#----------------------------------------------------------------#
+
+#import collections
+
+#d = collections.deque([1, 2, 3, 4, 5, 6, 7, 8])
+#d.rotate(4) # vyrti pyrvite 4 elementa
+#print(d)
+# deque([5, 6, 7, 8, 1, 2, 3, 4])
+
---
+++ Lab/U17052022.py
@@ -0,0 +1,216 @@
+
+
+# 1
+# numbers = input().split(' ')
+#
+# while numbers:
+#     print(numbers.pop(), end=' ')
+
+# 2
+# s = []
+# n = int(input())
+#
+# for _ in range(n):
+#     query = input().split(' ')
+#     action = query[0]
+#
+#     if action == '1':
+#         s.append(int(query[1]))
+#
+#     elif action == '2' and s:   #ako steka ne e prazen inache greshka
+#         s.pop()
+#
+#     elif action == '3' and s:
+#         print(max(s))
+#
+#     elif action == '4' and s:
+#         print(min(s))
+#
+# reversed_stack = []
+# while s:
+#     reversed_stack.append(str(s.pop()))
+#
+# print(', '.join(reversed_stack))    #ne raboteshe zashtoto vytre sa chisla a ne string!!!
+
+# 3 sam - queue
+# import collections
+#
+# q = collections.deque()
+# food_quantity = int(input())
+# order_sequence = [int(x) for x in input().split()]
+#
+# for i in order_sequence:
+#     q.append(i)
+#
+# print(max(q))
+#
+# failed = False
+#
+# while q:
+#     current_serving = q[0]
+#
+#     if current_serving <= food_quantity:
+#         food_quantity -= current_serving
+#         q.popleft()
+#     else:
+#         failed = True
+#         break
+#
+# if not failed:
+#     print("Orders complete")
+# else:
+#     print("Orders left:", end=' ')
+#     for k in q:
+#         print(k, end=' ')
+
+
+# 4
+# stack = [int(x) for x in input().split()]
+# base_capacity = int(input())
+# rack_counter = 1
+# capacity_left = base_capacity
+#
+# while stack:
+#     next_clothes = stack[-1]  # peek operaciq
+#
+#     if next_clothes > capacity_left:
+#         rack_counter += 1
+#         capacity_left = base_capacity
+#
+#     else:
+#         capacity_left -= next_clothes
+#         stack.pop()
+#
+# print(rack_counter)
+
+# 5 sys queue -
+# from collections import deque
+#
+# q = deque()
+# pumps_count = int(input())
+#
+# for i in range(pumps_count):
+#     q.append([int(x) for x in input().split()])     #vkarvame gi na masivcheta
+#
+# for attempt in range(pumps_count):
+#     tank = 0
+#     failed = False
+#     for petrol, distance in q:      #unpacking
+#         tank = tank + petrol - distance
+#         if tank < 0:
+#             failed = True
+#             break
+#
+#     if failed:
+#         q.append(q.popleft())
+#     else:
+#         print(attempt)
+#         break
+
+#----------------------------------------------------------------#
+
+#for, else
+#else se izpylnqva sled kraq na for, ako predi tova nqma break
+
+#----------------------------------------------------------------#
+
+# 6 - -1.28
+# stack = []
+# sequence = input()
+# counter = 0
+#
+# for i in range(len(sequence)):
+#     stack.append(sequence[i])
+#
+#     if stack[counter] == '}' and stack[counter-1] == '{':
+#         stack.pop()
+#         stack.pop()
+#         counter -= 2
+#
+#     elif stack[counter] == ']' and stack[counter-1] == '[':
+#         stack.pop()
+#         stack.pop()
+#         counter -= 2
+#
+#     elif stack[counter] == ')' and stack[counter-1] == '(':
+#         stack.pop()
+#         stack.pop()
+#         counter -= 2
+#
+#     counter += 1
+#
+# if len(stack) > 0:
+#     print('NO')
+# else:
+#     print('YES')
+
+# 7 sam na 25%
+# from collections import deque
+#
+# command = input().split(';')
+# robots = {}
+# for i in command:
+#     explode = i.split('-')
+#     name = explode[0]
+#     processing_time = int(explode[1])
+#     robots[name] = [processing_time, 'free', 0]
+#
+# starting_time = input().split(':')
+# hh = int(starting_time[0])
+# mm = int(starting_time[1])
+# ss = int(starting_time[2])
+# elapsed_seconds = 0
+#
+# waiting_list = deque()
+#
+# while 1:
+#
+#     if len(waiting_list) > 0:
+#         entry = waiting_list.popleft()
+#
+#     else:
+#         entry = input()
+#         if entry == 'End':
+#             break
+#
+#     job_started = False
+#     current_product = entry
+#     elapsed_seconds += 1
+#
+#     for k in robots.keys():
+#         if robots[k][2] <= elapsed_seconds:
+#             robots[k][1] = 'free'
+#
+#     for j in robots.keys():
+#         if robots[j][1] == 'free':
+#             robots[j][1] = 'occupied'
+#             job_started = True
+#
+#             robots[j][2] = elapsed_seconds + robots[j][0]
+#
+#             ss_to_print = ss + elapsed_seconds
+#             mm_to_print = mm
+#             hh_to_print = hh
+#             if ss_to_print > 59:
+#                 ss_to_print -= 60
+#                 mm_to_print += 1
+#             if mm_to_print > 59:
+#                 mm_to_print -= 60
+#                 hh_to_print += 1
+#
+#             print(f'{j} - {current_product} [{hh_to_print:02d}:{mm_to_print:02d}:{ss_to_print:02d}]')
+#
+#             #print(f'{j} - {current_product} [{elapsed_seconds}]')
+#             break
+#
+#     if not job_started:
+#         waiting_list.append(current_product)
+
+
+
+
+
+
+
+
+
