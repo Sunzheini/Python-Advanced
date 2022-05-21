a8067a4091899defc3dc1505954c3c3bf97dee54
Sunzheini <daniel_zorov@abv.bg>
2022-05-21 16:58:50 +0300

    Update L19052022.py

--- Lab/L19052022.py
+++ Lab/L19052022.py
@@ -5,10 +5,10 @@
 # tt = (1, 2, 3, 4)
 # print(tt)
 # print(tt[1])
-#nqmame append insert pop i tn
-#moje prezapisvane tt = (1, 2, 4)
+# nqmame append insert pop i tn
+# moje prezapisvane tt = (1, 2, 4)
 
-#definiciq
+# definiciq
 # t = (1, 2, 3)
 # t = 1, 2, 3
 # t = (1, ) # s edin element
@@ -21,7 +21,7 @@
 # print(tuple(ll))
 
 
-#methods
+# methods
 # numbers = (1, 2, 3, 4)
 # print(numbers.count(1))
 
@@ -30,7 +30,7 @@
 # print(names.index('morz', 1)) # index ('morz', 1) tyrsi morz ot 1vi index nadqsno
 
 
-#oshte operacii
+# oshte operacii
 # tt = (1, 2, 3, 4, 5)
 #
 # print(1 in tt) # True
@@ -61,7 +61,7 @@
 #     print(f'dd[{key}]={value}')
 
 
-#unpacking works for lists too
+# unpacking works for lists too
 # ll = [1, 2, 3]
 # a, b, c = ll
 
@@ -108,8 +108,7 @@
 # --------------------------------------------------------------------#
 
 
-
-#SETS
+# SETS
 # --------------------------------------------------------------------#
 # search, add, remove is very fast
 # contain only unique values
@@ -146,40 +145,90 @@
 # --------------------------------------------------------------------#
 
 # 3
-n = int(input())
+# n = int(input())
+#
+# names_set = set()
+#
+# for _ in range(n):
+#     names_set.add(input())
+#
+# [print(name) for name in names_set]     # !!!
 
-names_set = set()
+# 4
+# number = int(input())
+# parking_lot = set()
+#
+# commands = [input().split(', ') for i in range(number)]
+#
+# for direction, car_number in commands:
+#     if direction == 'IN':
+#         parking_lot.add(car_number)
+#     elif direction == 'OUT':
+#         parking_lot.remove(car_number)
+#
+# if parking_lot:
+#     [print(car_number) for car_number in parking_lot]
+# else:
+#     print('Parking Lot is Empty')
 
-for _ in range(n):
-    names_set.add(input())
+# 5
+# def is_vip(guest):
+#     return guest[0].isdigit()
+#
+#
+# n = int(input())
+# vip_guests = set()
+# regular_guests = set()
+#
+# for i in range(n):
+#     current_guest = input()
+#     if is_vip(current_guest):
+#         vip_guests.add(current_guest)
+#     else:
+#         regular_guests.add(current_guest)
+#
+# while 1:
+#     command = input()
+#     if command == 'END':
+#         break
+#
+#     if is_vip(command):
+#         vip_guests.remove(command)
+#     else:
+#         regular_guests.remove(command)
+#
+# print(f'{len(vip_guests) + len(regular_guests)}')
+# [print(x) for x in sorted(vip_guests)]
+# [print(x) for x in sorted(regular_guests)]
 
-[print(name) for name in names_set]     # !!!
+# --------------------------------------------------------------------#
+# from random import shuffle
+#
+# print(shuffle(ll))
+# --------------------------------------------------------------------#
 
-#4
-number = int(input())
-parking_lot = set()
+# 6 - sam
+#1 5 4 2 2 3 1 3 2 -> value
+#0 1 2 3 4 5 6 7 8 -> pos
 
-commands = [input().split(', ') for i in range(number)]
+number_sequence = list(map(int, input().split(' ')))
+target_sum = int(input())
+set_of_numbers = set()
+iterations = 0
 
-for direction, car_number in commands:
-    if direction == 'IN':
-        parking_lot.add(car_number)
-    elif direction == 'OUT':
-        parking_lot.remove(car_number)
+for i in range(len(number_sequence)):
+    for j in range(len(number_sequence)):
+        if i < j:
+            iterations += 1
+            if number_sequence[i] + number_sequence[j] == target_sum:
+                print(f'{number_sequence[i]} + {number_sequence[j]} = {target_sum}')
 
-if parking_lot:
-    [print(car_number) for car_number in parking_lot]
-else:
-    print('Parking Lot is Empty')
+                current_tuple = (number_sequence[i], number_sequence[j])
 
-# 5 -0.42 v 9.41h
+                set_of_numbers.add(current_tuple)
 
-
-
-
-
-
-
+print(f'Iterations done: {iterations}')
+[print(x) for x in set_of_numbers]
 
 
 
