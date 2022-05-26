3fd592bafb066f10f130b1f3930f17cddfcdd61c
Daniel Zorov <daniel_zorov@abv.bg>
2022-05-26 20:30:40 +0300

    Update U25052022.py

--- Lab/U25052022.py
+++ Lab/U25052022.py
@@ -63,61 +63,189 @@
 # print(q.popleft())
 
 # 3
+# from collections import deque
+#
+# chocolates = [int(x) for x in input().split(', ')]
+# cups_of_milk = deque([int(x) for x in input().split(', ')])
+# milkshakes = 0
+#
+# while chocolates and cups_of_milk and milkshakes < 5:
+#     current_chocolate = chocolates.pop()
+#     current_cup_of_milk = cups_of_milk.popleft()
+#
+#     if current_chocolate <= 0 and current_cup_of_milk <= 0:
+#         continue
+#
+#     if current_chocolate <= 0:
+#         cups_of_milk.appendleft(current_cup_of_milk)
+#         continue
+#
+#     if current_cup_of_milk <= 0:
+#         chocolates.append(current_chocolate)
+#         continue
+#
+#     if current_chocolate == current_cup_of_milk:
+#         milkshakes += 1
+#
+#     else:
+#         cups_of_milk.append(current_cup_of_milk)
+#         current_chocolate -= 5
+#         chocolates.append(current_chocolate)
+#
+# if milkshakes == 5:
+#     print("Great! You made all the chocolate milkshakes needed!")
+# else:
+#     print("Not enough milkshakes.")
+#
+# if len(chocolates) > 0:
+#     print(f"Chocolate: {', '.join(map(str, chocolates))}")
+# else:
+#     print("Chocolate: empty")
+#
+# if len(cups_of_milk) > 0:
+#     print(f"Milk: {', '.join(map(str, cups_of_milk))}")
+# else:
+#     print("Milk: empty")
+
+# 4
+# from collections import deque
+#
+# working_bees = deque([int(x) for x in input().split()])
+# nectar = [int(x) for x in input().split()]
+# process = deque([x for x in input().split()])
+# honey = 0
+#
+# while len(working_bees) > 0 and len(nectar) > 0:
+#     current_bee = working_bees.popleft()
+#     current_nectar = nectar.pop()
+#
+#     if current_bee > current_nectar:
+#         working_bees.appendleft(current_bee)
+#         continue
+#
+#     if current_nectar == 0:         #zabravih delenie na 0!
+#         continue
+#
+#     current_process = process.popleft()
+#
+#     if current_process == '+':
+#         honey += (current_bee + current_nectar)
+#     elif current_process == '-':
+#         honey += abs(current_bee - current_nectar)
+#     elif current_process == '*':
+#         honey += (current_bee * current_nectar)
+#     elif current_process == '/':
+#         honey += (current_bee / current_nectar)
+#
+# print(f"Total honey made: {honey}")
+#
+# if len(working_bees) > 0:
+#     print(f"Bees left: {', '.join(map(str, working_bees))}")
+# if len(nectar) > 0:
+#     print(f"Nectar left: {', '.join(map(str, nectar))}")
+
+# 5
+# from collections import deque
+#
+# materials = [int(x) for x in input().split()]
+# magic_levels = deque([int(x) for x in input().split()])
+# crafting_table = {150: 'Doll', 250: 'Wooden train',
+#         300: 'Teddy bear', 400: 'Bicycle'}
+# toys = {}
+#
+# while materials and magic_levels:
+#     current_materials = materials.pop()
+#     current_magic_level = magic_levels.popleft()
+#
+#     if current_materials == 0 and current_magic_level == 0:
+#         continue
+#
+#     if current_materials == 0:
+#         magic_levels.appendleft(current_magic_level)
+#         continue            #zabravih continue
+#
+#     if current_magic_level == 0:
+#         materials.append(current_materials)
+#         continue
+#
+#     total_magic_level = current_materials * current_magic_level
+#
+#     if total_magic_level in crafting_table.keys():
+#         toy_name = crafting_table[total_magic_level]
+#         if toy_name not in toys.keys():
+#             toys[toy_name] = 0
+#         toys[toy_name] += 1
+#         continue
+#
+#     if total_magic_level < 0:
+#         junk = current_materials + current_magic_level
+#         materials.append(junk)
+#     else:
+#         materials.append(current_materials + 15)
+#
+# if 'Doll' in toys and 'Wooden train' in toys:
+#     print("The presents are crafted! Merry Christmas!")
+# elif 'Teddy bear' in toys and 'Bicycle' in toys:
+#     print("The presents are crafted! Merry Christmas!")
+# else:
+#     print("No presents this Christmas!")
+#
+# if materials:
+#     print(f"Materials left: {', '.join([str(x) for x in reversed(materials)])}")
+# if magic_levels:
+#     print(f"Magic left: {', '.join([str(x) for x in magic_levels])}")
+#
+# for i in sorted(toys.keys()):
+#     print(f"{i}: {toys[i]}")
+
+# 6
 from collections import deque
 
-chocolates = [int(x) for x in input().split(', ')]
-cups_of_milk = deque([int(x) for x in input().split(', ')])
-milkshakes = 0
+words = deque(input().split())
+main_colors = {'red', 'yellow', 'blue'}
+secondary_colors = {'orange', 'purple', 'green'}
+collected_colors = []
 
-while chocolates and cups_of_milk and milkshakes < 5:
-    current_chocolate = chocolates.pop()
-    current_cup_of_milk = cups_of_milk.popleft()
+while words:
+    left = words.popleft()
+    right = words.pop() if words else ''  #!ako e ostanal element
 
-    if current_chocolate <= 0 and current_cup_of_milk <= 0:
+    result = left + right
+    if result in main_colors or result in secondary_colors:
+        collected_colors.append(result)
         continue
 
-    if current_chocolate <= 0:
-        cups_of_milk.appendleft(current_cup_of_milk)
+    result = right + left
+    if result in main_colors or result in secondary_colors:
+        collected_colors.append(result)
         continue
 
-    if current_cup_of_milk <= 0:
-        chocolates.append(current_chocolate)
+    left = left[:-1]
+    right = right[:-1]
+
+    if left:
+        words.insert(len(words) // 2, left)
+    if right:
+        words.insert(len(words) // 2, right)
+
+result = []
+forming_colors = {'orange': ['red', 'yellow'],
+                  'purple': ['red', 'blue'],
+                  'green': ['yellow', 'blue']}
+
+for color in collected_colors:
+    if color in main_colors:
+        result.append(color)
         continue
 
-    if current_chocolate == current_cup_of_milk:
-        milkshakes += 1
+    is_collected = True
+    for helping_color in forming_colors[color]:
+        if helping_color not in collected_colors:
+            is_collected = False
+            break
 
-    else:
-        cups_of_milk.append(current_cup_of_milk)
-        current_chocolate -= 5
-        chocolates.append(current_chocolate)
+    if is_collected:
+        result.append(color)
 
-if milkshakes == 5:
-    print("Great! You made all the chocolate milkshakes needed!")
-else:
-    print("Not enough milkshakes.")
-
-if len(chocolates) > 0:
-    print(f"Chocolate: {', '.join(map(str, chocolates))}")
-else:
-    print("Chocolate: empty")
-
-if len(cups_of_milk) > 0:
-    print(f"Milk: {', '.join(map(str, cups_of_milk))}")
-else:
-    print("Milk: empty")
-
-# 4 -1.45h
-
-
-
-
-
-
-
-
-
-
-
-
+print(result)
 
