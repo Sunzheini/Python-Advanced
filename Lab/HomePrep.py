8e2c8ca73c3640f87eb35f4aeaf2a50ba1d3bec7
Daniel Zorov <daniel_zorov@abv.bg>
2022-06-22 19:54:55 +0300

    Update HomePrep.py

--- Lab/HomePrep.py
+++ Lab/HomePrep.py
@@ -33,31 +33,166 @@
 #     if customers:
 #         print(f"Customers left: {', '.join(map(str, customers))}")
 
+
+
 # 2. North Pole Challenge
+def y_square(matrix, r, c):
+    row, col = 0, 0
+    for j in range(r):
+        for k in range(c):
+            if matrix[j][k] == 'Y':
+                row = j
+                col = k
+    return row, col
+
+
 rows, columns = [int(x) for x in input().split(', ')]
 workshop_matrix = []
-player_row = 0
-player_col = 0
+items = {'Christmas decorations': 0, 'Gifts': 0, 'Cookies': 0}
+collected_all = False
 
 for i in range(rows):
     current_line = input().split(' ')
     workshop_matrix.append(current_line)
 
+player_row, player_col = y_square(workshop_matrix, rows, columns)
+
 while 1:
-    command = input()
-    if command == "End":
+
+    if collected_all:
+        print("Merry Christmas!")
+        workshop_matrix[player_row][player_col] = 'Y'
         break
 
-print(workshop_matrix)
+    command = input()
+    if command == "End":
+        workshop_matrix[player_row][player_col] = 'Y'
+        break
+
+    explode = command.split('-')
+    direction = explode[0]
+    steps = int(explode[1])
+
+    workshop_matrix[player_row][player_col] = 'x'
+
+# --------------------------------------------------------------------------------------------------
+
+    if direction == 'left':
+
+        for step in range(steps):
+
+            if collected_all:
+                break
+
+            player_col -= 1
+            if player_col < 0:
+                player_col = columns - 1
+
+            if workshop_matrix[player_row][player_col] == 'D':
+                items['Christmas decorations'] += 1
+            elif workshop_matrix[player_row][player_col] == 'G':
+                items['Gifts'] += 1
+            elif workshop_matrix[player_row][player_col] == 'C':
+                items['Cookies'] += 1
+
+            workshop_matrix[player_row][player_col] = 'x'
+
+            collected_all = True
+            for check_row in range(rows):
+                for check_col in range(columns):
+                    if workshop_matrix[check_row][check_col] in "GCD":
+                        collected_all = False
 
 
+# --------------------------------------------------------------------------------------------------
+
+    if direction == 'right':
+        for step in range(steps):
+
+            if collected_all:
+                break
+
+            player_col += 1
+            if player_col > columns - 1:
+                player_col = 0
+
+            if workshop_matrix[player_row][player_col] == 'D':
+                items['Christmas decorations'] += 1
+            elif workshop_matrix[player_row][player_col] == 'G':
+                items['Gifts'] += 1
+            elif workshop_matrix[player_row][player_col] == 'C':
+                items['Cookies'] += 1
+
+            workshop_matrix[player_row][player_col] = 'x'
+
+            collected_all = True
+            for check_row in range(rows):
+                for check_col in range(columns):
+                    if workshop_matrix[check_row][check_col] in "GCD":
+                        collected_all = False
+
+# --------------------------------------------------------------------------------------------------
+
+    if direction == 'up':
+        for step in range(steps):
+
+            if collected_all:
+                break
+
+            player_row -= 1
+            if player_row < 0:
+                player_row = rows - 1
+
+            if workshop_matrix[player_row][player_col] == 'D':
+                items['Christmas decorations'] += 1
+            elif workshop_matrix[player_row][player_col] == 'G':
+                items['Gifts'] += 1
+            elif workshop_matrix[player_row][player_col] == 'C':
+                items['Cookies'] += 1
+
+            workshop_matrix[player_row][player_col] = 'x'
+
+            collected_all = True
+            for check_row in range(rows):
+                for check_col in range(columns):
+                    if workshop_matrix[check_row][check_col] in "GCD":
+                        collected_all = False
+
+# --------------------------------------------------------------------------------------------------
+
+    if direction == 'down':
+        for step in range(steps):
+
+            if collected_all:
+                break
+
+            player_row += 1
+            if player_row > rows - 1:
+                player_row = 0
+
+            if workshop_matrix[player_row][player_col] == 'D':
+                items['Christmas decorations'] += 1
+            elif workshop_matrix[player_row][player_col] == 'G':
+                items['Gifts'] += 1
+            elif workshop_matrix[player_row][player_col] == 'C':
+                items['Cookies'] += 1
+
+            workshop_matrix[player_row][player_col] = 'x'
+
+            collected_all = True
+            for check_row in range(rows):
+                for check_col in range(columns):
+                    if workshop_matrix[check_row][check_col] in "GCD":
+                        collected_all = False
 
 
+print("You've collected:")
+for key in items.keys():
+    print(f"- {items[key]} {key}")
 
 
-
-
-
+for line in workshop_matrix:
+    print(f"{' '.join(line)}")
 
 
 
